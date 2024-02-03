42547c360311bd657abbf4c3addf23b89deb7c91
Joshua <abrahamj101@tamu.edu>
2024-02-03 14:36:17 -0600

    more updates

--- toggle.js
+++ toggle.js
@@ -1,23 +1,17 @@
 // This function toggles between two stylesheet files
 function toggleStyleSheet() {
-    // Get the style element by its ID
     const styleElement = document.getElementById("mainStyleSheet");
-    // Check the current stylesheet file name
     const currStyle = styleElement.getAttribute("href");
-    // Determine the new stylesheet file name
     const newStyle = (currStyle === "style.css") ? "style2.css" : "style.css";
-    // Replace the stylesheet with the new stylesheet
     styleElement.setAttribute("href", newStyle);
-    // Save the new stylesheet name to localStorage for persistence
-    localStorage.setItem("currentStyle", newStyle);
+    localStorage.setItem("stylesheet", newStyle);
 }
 
 // This function is triggered when the window loads
 window.onload = function() {
-    // Get the saved stylesheet name from localStorage
-    const savedStyle = localStorage.getItem("currentStyle") || "style.css";
-    // Get the HTML style element by its ID
-    const styleElement = document.getElementById("mainStyleSheet");
-    // Replace the href attribute of the HTML element with the saved style
+    var savedStyle = localStorage.getItem("stylesheet") || "style.css"; //checks which style is selected rn
+    var styleElement = document.getElementById("mainStyleSheet");
     styleElement.setAttribute("href", savedStyle);
-}
+    document.getElementById('switchStyle').addEventListener('click', toggleStyleSheet); //swticher
+};
+
